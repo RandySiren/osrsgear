1f5b8f588a00ca6d7834b128652651d1781da8a2
RandySiren <sranmandeep1997@gmail.com>
2020-08-26 02:27:56 -0400

    More work on autocomplete, renders conditionally and based on input, no styling done

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import classes from './SearchBar.module.css';
 
@@ -11,14 +11,20 @@ const SearchBar: React.FC<SearchBarProps> = ({
     autocompleteItems,
     onInputChange,
 }) => {
-    let autocomplete: React.ReactNode | null = null;
+    let suggestions: React.ReactNode | null = null;
     if (autocompleteItems) {
-        autocomplete = <div></div>;
+        suggestions = (
+            <ul className={classes.Suggestions}>
+                {autocompleteItems.map((item, index) => {
+                    return <li key={index}>{item}</li>;
+                })}
+            </ul>
+        );
     }
     return (
         <div className={classes.SearchBar}>
             <input type='text' onChange={onInputChange} />
-            {autocomplete}
+            {suggestions}
         </div>
     );
 };
--- src/containers/MainContainer/MainContainer.tsx
+++ src/containers/MainContainer/MainContainer.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import classes from './MainContainer.module.css';
 
 import SearchBar from '../../components/SearchBar';
@@ -6,15 +6,26 @@ import SearchBar from '../../components/SearchBar';
 export interface MainContainerProps {}
 
 const MainContainer: React.SFC<MainContainerProps> = () => {
+    const [input, setInput] = useState<string | null>(null); // Search bar input state
+    const [suggestions, setSuggestions] = useState<string[]>([]); // Autocomplete suggestions array
+
+    // Search bar input change handler
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
+        const inputValue = e.target.value;
+
+        setInput(inputValue);
+
+        if (inputValue) {
+            const newSuggestions: string[] = inputValue.split('');
+            setSuggestions(newSuggestions);
+        }
     };
 
     return (
         <div className={classes.MainContainer}>
             <SearchBar
                 onInputChange={onInputChange}
-                autocompleteItems={['1', '2', '3', '4', '5']}
+                autocompleteItems={suggestions}
             />
         </div>
     );
